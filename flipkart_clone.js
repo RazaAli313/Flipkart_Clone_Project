826333dd7f59758242da44cc12296ec87bae13d6
RazaAli313 <136004622+RazaAli313@users.noreply.github.com>
2023-08-02 15:54:48 +0500

    Update flipkart_clone.js

---
+++ flipkart_clone.js
@@ -0,0 +1,16 @@
+addEventListener('load',function(){
+  window.alert("Welcome to Flipkart");
+})
+btn.style.width="100px";
+btn.addEventListener('click',function(){
+  if( btn.style.width==="100px"){
+    btn.style.width="70px";}
+ else  if( btn.style.width==="70px"){
+     btn.style.width="100px";}
+})
+btn3.addEventListener('mouseover',function(){
+    alert("Get Extra Discount");
+})
+btn1.addEventListener('mouseover',function(){
+    alert("Sell your goods and earn healthy");
+})
